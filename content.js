ece73d75db073b50c11a5d079165a8b1f717f2ef
Sindre Sorhus <sindresorhus@gmail.com>
2014-08-03 20:12:44 +0200

    codestyle and cleanup

--- content.js
+++ content.js
@@ -3,43 +3,45 @@
 var hidden = true;
 var isDotfilesRepository = document.querySelector('.js-current-repository[href$="/dotfiles"]');
 
+function createHtml(str) {
+	var frag = document.createDocumentFragment();
+	var temp = document.createElement('div');
+
+	temp.innerHTML = str;
+
+	while (temp.firstChild) {
+		frag.appendChild(temp.firstChild);
+	}
+
+	return frag;
+}
+
 function toggleFiles() {
 	[].forEach.call(document.querySelectorAll('.files tr'), function (el) {
 		if (el.querySelector('a[title^="."')) {
 			el.style.display = hidden ? 'none' : 'table-row';
-		};
+		}
 	});
 }
 
 function addToggleBtn() {
-
-	var _createHTML = function (htmlStr) {
-		var frag = document.createDocumentFragment(),
-		temp = document.createElement('a');
-		temp.innerHTML = htmlStr;
-		while (temp.firstChild) {
-			frag.appendChild(temp.firstChild);
-		}
-		return frag;
-	};
-
-	var toggleBtn = _createHTML("<a class='minibutton hide-files-btn' style='margin-right:8px'>Toggle dotfiles</a>")
-	var btnLocation = document.querySelector(".file-navigation .breadcrumb");
+	var toggleBtn = createHtml('<a class="minibutton hide-files-btn" style="margin-right:8px">Toggle dotfiles</a>');
+	var btnLocation = document.querySelector('.file-navigation .breadcrumb');
 	var toogleBtnCached = document.querySelector('.hide-files-btn');
 
-	if (!toogleBtnCached) btnLocation.parentNode.insertBefore(toggleBtn, btnLocation);
-	
-	document.querySelector('.hide-files-btn').addEventListener("click", function () {
+	if (!toogleBtnCached) {
+		btnLocation.parentNode.insertBefore(toggleBtn, btnLocation);
+	}
+
+	document.querySelector('.hide-files-btn').addEventListener('click', function () {
 		hidden = !hidden;
 		toggleFiles();
 	});
-
-	toggleFiles();
 }
 
 if (!isDotfilesRepository) {
-	
 	addToggleBtn();
+	toggleFiles();
 
 	new MutationObserver(addToggleBtn).observe(document.querySelector('#js-repo-pjax-container'), {childList: true});
 }
