3fc1e6fbf409fa110e17b6d5128c96b892139e39
Ian Wang <gogoenwon@gmail.com>
2014-08-03 22:36:40 +0800

    implement toggle function

--- content.js
+++ content.js
@@ -1,16 +1,49 @@
 'use strict';
 
-function hideFiles() {
+var hidden = true;
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+var isDotfilesRepository = document.querySelector('.js-current-repository[href$="/dotfiles"]');
+
+function toggleFiles() {
 	[].forEach.call(document.querySelectorAll('.files tr'), function (el) {
-		if (el.querySelector('a[title^="."')) {
-			el.remove();
-		}
+		if(el.querySelector('a[title^="."')) {
+			el.style.display = hidden ? 'none' : 'table-row';
+		};
 	});
 }
 
-var isDotfilesRepository = document.querySelector('.js-current-repository[href$="/dotfiles"]');
+function trigger() {
+	hidden = !hidden;
+	toggleFiles();
+}
+
+function addToggleBtn(ee) {
+	var _createHTML = function(htmlStr) {
+	    var frag = document.createDocumentFragment(),
+	        temp = document.createElement('a');
+	    temp.innerHTML = htmlStr;
+	    while (temp.firstChild) {
+	        frag.appendChild(temp.firstChild);
+	    }
+	    return frag;
+	}
+
+	var toggleBtn = _createHTML("<a class='minibutton hide-file-toggle' style='margin-right:8px'>Toggle dotfiles</a>")
+	var btnLocation = document.querySelector(".file-navigation .breadcrumb");
+	var toogleBtnCached = document.querySelector('.hide-file-toggle');
+
+	if(!toogleBtnCached) btnLocation.parentNode.insertBefore(toggleBtn, btnLocation);
+	
+	document.querySelector('.hide-file-toggle').addEventListener("click", trigger);
+
+	toggleFiles();
+}
+
+
 
 if (!isDotfilesRepository) {
-	hideFiles();
-	new MutationObserver(hideFiles).observe(document.querySelector('#js-repo-pjax-container'), {childList: true});
+	
+	addToggleBtn();
+
+	new MutationObserver(addToggleBtn).observe(document.querySelector('#js-repo-pjax-container'), {childList: true});
 }
