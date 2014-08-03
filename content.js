6421a0a96b5724a815fee11cd4daf74fda22d45c
Ian Wang <gogoenwon@gmail.com>
2014-08-03 23:37:59 +0800

    fix feedback

--- content.js
+++ content.js
@@ -1,45 +1,41 @@
 'use strict';
 
 var hidden = true;
-var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 var isDotfilesRepository = document.querySelector('.js-current-repository[href$="/dotfiles"]');
 
 function toggleFiles() {
 	[].forEach.call(document.querySelectorAll('.files tr'), function (el) {
-		if(el.querySelector('a[title^="."')) {
+		if (el.querySelector('a[title^="."')) {
 			el.style.display = hidden ? 'none' : 'table-row';
 		};
 	});
 }
 
-function trigger() {
-	hidden = !hidden;
-	toggleFiles();
-}
+function addToggleBtn() {
 
-function addToggleBtn(ee) {
-	var _createHTML = function(htmlStr) {
-	    var frag = document.createDocumentFragment(),
-	        temp = document.createElement('a');
-	    temp.innerHTML = htmlStr;
-	    while (temp.firstChild) {
-	        frag.appendChild(temp.firstChild);
-	    }
-	    return frag;
-	}
+	var _createHTML = function (htmlStr) {
+	  var frag = document.createDocumentFragment(),
+	  temp = document.createElement('a');
+	  temp.innerHTML = htmlStr;
+	  while (temp.firstChild) {
+	    frag.appendChild(temp.firstChild);
+	  }
+	  return frag;
+	};
 
-	var toggleBtn = _createHTML("<a class='minibutton hide-file-toggle' style='margin-right:8px'>Toggle dotfiles</a>")
+	var toggleBtn = _createHTML("<a class='minibutton hide-files-btn' style='margin-right:8px'>Toggle dotfiles</a>")
 	var btnLocation = document.querySelector(".file-navigation .breadcrumb");
-	var toogleBtnCached = document.querySelector('.hide-file-toggle');
+	var toogleBtnCached = document.querySelector('.hide-files-btn');
 
-	if(!toogleBtnCached) btnLocation.parentNode.insertBefore(toggleBtn, btnLocation);
+	if (!toogleBtnCached) btnLocation.parentNode.insertBefore(toggleBtn, btnLocation);
 	
-	document.querySelector('.hide-file-toggle').addEventListener("click", trigger);
+	document.querySelector('.hide-files-btn').addEventListener("click", function () {
+	  hidden = !hidden;
+	  toggleFiles();
+  });
 
 	toggleFiles();
 }
-
-
 
 if (!isDotfilesRepository) {
 	
