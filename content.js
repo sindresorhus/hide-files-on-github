3cd0cab46014d5bd25a6ea32a0b1f73b59446a7b
Ian Wang <gogoenwon@gmail.com>
2014-08-03 20:04:43 +0200

    Close GH-6: implement toggle function. Fixes #1

--- content.js
+++ content.js
@@ -1,16 +1,45 @@
 'use strict';
 
-function hideFiles() {
+var hidden = true;
+var isDotfilesRepository = document.querySelector('.js-current-repository[href$="/dotfiles"]');
+
+function toggleFiles() {
 	[].forEach.call(document.querySelectorAll('.files tr'), function (el) {
 		if (el.querySelector('a[title^="."')) {
-			el.remove();
-		}
+			el.style.display = hidden ? 'none' : 'table-row';
+		};
 	});
 }
 
-var isDotfilesRepository = document.querySelector('.js-current-repository[href$="/dotfiles"]');
+function addToggleBtn() {
+
+	var _createHTML = function (htmlStr) {
+		var frag = document.createDocumentFragment(),
+		temp = document.createElement('a');
+		temp.innerHTML = htmlStr;
+		while (temp.firstChild) {
+			frag.appendChild(temp.firstChild);
+		}
+		return frag;
+	};
+
+	var toggleBtn = _createHTML("<a class='minibutton hide-files-btn' style='margin-right:8px'>Toggle dotfiles</a>")
+	var btnLocation = document.querySelector(".file-navigation .breadcrumb");
+	var toogleBtnCached = document.querySelector('.hide-files-btn');
+
+	if (!toogleBtnCached) btnLocation.parentNode.insertBefore(toggleBtn, btnLocation);
+	
+	document.querySelector('.hide-files-btn').addEventListener("click", function () {
+		hidden = !hidden;
+		toggleFiles();
+	});
+
+	toggleFiles();
+}
 
 if (!isDotfilesRepository) {
-	hideFiles();
-	new MutationObserver(hideFiles).observe(document.querySelector('#js-repo-pjax-container'), {childList: true});
+	
+	addToggleBtn();
+
+	new MutationObserver(addToggleBtn).observe(document.querySelector('#js-repo-pjax-container'), {childList: true});
 }
