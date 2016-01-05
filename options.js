b2669baf1e1ad396e437e523482921ab0c25e0d6
Michael Wuergler <wuergler@gmail.com>
2016-01-05 12:15:43 +0000

    adds dimming option in options page. Closes #3

--- options.js
+++ options.js
@@ -1,44 +1,45 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    var saveBtn = document.getElementById('saveBtn');
+	const saveBtn = document.getElementById('saveBtn');
 
-    if (saveBtn) {
-        saveBtn.addEventListener('click', saveOptions);
-    }
-
-    restoreOptions();
+	if (saveBtn) {
+		saveBtn.addEventListener('click', saveOptions);
+	}
+	restoreOptions();
 });
 
 // Saves options to chrome.storage
 function saveOptions() {
-    var visibility = document.querySelector('input[name=visibilityOption]:checked').value;
-    var ignoreRegEx = document.getElementById('ignoreRegExp').value;
+	const visibility = document.querySelector('input[name=visibilityOption]:checked').value;
+	const ignoreRegEx = document.getElementById('ignoreRegExp').value;
 
-    chrome.storage.sync.set({
-        HFOG_VISIBILITY: visibility,
-        HFOG_IGNOREREGEX: ignoreRegEx
-    }, () => {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
-    });
+	window.chrome.storage.sync.set({
+		HFOG_VISIBILITY: visibility,
+		HFOG_IGNOREREGEX: ignoreRegEx
+	}, () => {
+		// Update status to let user know options were saved.
+		const status = document.getElementById('status');
+
+		status.textContent = 'Options saved.';
+		setTimeout(() => {
+			status.textContent = '';
+		}, 750);
+	});
 }
 
 function restoreOptions() {
-    // Use default values: { visibility: 'hidden', HFOGIgnoreRegExp: '' }
-    chrome.storage.sync.get({
-        HFOG_VISIBILITY: 'hidden',
-        HFOG_IGNOREREGEX: ''
-    }, items => {
-        // visibility
-        document.getElementById(items.HFOG_VISIBILITY).checked = true;
+	// Use default values: { visibility: 'hidden', HFOGIgnoreRegExp: '' }
 
-        // regex
-        document.getElementById('ignoreRegExp').value = items.HFOG_IGNOREREGEX;
-    });
+	window.chrome.storage.sync.get({
+		HFOG_VISIBILITY: 'hidden',
+		HFOG_IGNOREREGEX: ''
+	}, items => {
+		// visibility
+		document.getElementById(items.HFOG_VISIBILITY).checked = true;
+
+		// regex
+		document.getElementById('ignoreRegExp').value = items.HFOG_IGNOREREGEX;
+	});
 }
 
