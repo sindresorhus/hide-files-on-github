1c9579c1d6aa3e5f5a979c387a351d51cbf00e89
Federico Brigante <github@bfred.it>
2017-07-20 12:06:00 +0800

    Show error instead of automatically correcting
    The correction moves the cursor to the end; bad UX

--- extension/options.js
+++ extension/options.js
@@ -1,20 +1,27 @@
 'use strict';
 const regexField = document.querySelector('#hideRegExp');
+const errorMessage = document.querySelector('#errorMessage');
 const delimiters = /^\/|\/$/;
 
 restoreOptions();
 regexField.addEventListener('input', update);
+
+/* Native validation tooltips don't seem to work */
+function setValidity(text = '') {
+	errorMessage.innerHTML = text;
+	regexField.setCustomValidity(text); /* Triggers :invalid */
+}
 
 function update() {
 	// Don't allow delimiters in RegExp string
 	const values = regexField.value.split('\n');
 	const hasDelimiters = values.some(line => delimiters.test(line));
 	if (hasDelimiters) {
-		regexField.value = values
-			.map(line => line.replace(delimiters, ''))
-			.join('\n');
+		return setValidity('Don’t use regex delimiters around each regex (type <code>a</code> instead of <code>/a/</code>)');
+	}
 	}
 
+	setValidity();
 	saveOptions();
 }
 
