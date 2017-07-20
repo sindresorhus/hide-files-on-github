e94df93e9feee2601da9752e554611daf76a646c
Federico Brigante <github@bfred.it>
2017-07-20 12:22:36 +0800

    Fully validate expressions and show error

--- extension/options.js
+++ extension/options.js
@@ -13,12 +13,19 @@ function setValidity(text = '') {
 }
 
 function update() {
-	// Don't allow delimiters in RegExp string
 	for (const line of regexField.value.split('\n')) {
+		// Don't allow delimiters in RegExp string
 		if (delimiters.test(line)) {
 			return setValidity(`Use <code>${line.replace(/^\/|\/$/g, '')}</code> instead of <code>${line}</code>. Slashes are not required.`);
 		}
-	}
+
+		// Fully test each RegExp
+		try {
+			// eslint-disable-next-line no-new
+			new RegExp(line);
+		} catch (err) {
+			return setValidity(err.message);
+		}
 	}
 
 	setValidity();
